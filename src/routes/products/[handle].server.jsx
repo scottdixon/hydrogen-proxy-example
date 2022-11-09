dbf7868a15f480f154d9544e5d5fd0b827140869
Scott Dixon <59898611+scottdixon@users.noreply.github.com>
2022-11-10 09:10:26 +1000

    Add User-Agent to proxy example

--- src/routes/products/[handle].server.jsx
+++ src/routes/products/[handle].server.jsx
@@ -1,9 +1,12 @@
 export async function api(request, { params }) {
-  // This method is only suitable for Oxygen
+  // This approach is only supported by Oxygen
   const { handle } = params;
-  const response = await fetch(
-    "https://example.myshopify.com/products/" + handle
-  );
+  const response = await fetch("https://example.myshopify.com/products/" + handle, {
+    headers: {
+      // It's important to set or forward a User-Agent, otherwise the Online Store might block the request
+      'User-Agent': 'Hydrogen',
+    },
+  });
   const data = await response.text();
   return new Response(data, { headers: { "content-type": "text/html" } });
 }
